ce478f4633fef61221376410a086a6d6588a69b1
Adrián <adrianheredev@gmail.com>
2026-02-27 10:55:17 +0100

    badges

--- src/components/GitHubStats/GitHubStats.tsx
+++ src/components/GitHubStats/GitHubStats.tsx
@@ -5,81 +5,88 @@ interface GithubStatsProps {
 }
 
 const badges = [
-  {
-    alt: 'CSS3',
-    class: 'badge',
-    src: 'https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
-  },
+  // --- FRONTEND ---
   {
     alt: 'HTML5',
     class: 'badge',
-    src: 'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+    src: 'https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
   },
   {
-    alt: 'Java',
+    alt: 'CSS3',
     class: 'badge',
-    src: 'https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white',
+    src: 'https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
   },
   {
     alt: 'JavaScript',
     class: 'badge',
-    src: 'https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E',
+    src: 'https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black',
   },
   {
     alt: 'TypeScript',
     class: 'badge',
-    src: 'https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white',
-  },
-  {
-    alt: 'XML', 
-    class: 'badge',
-    src: 'https://img.shields.io/badge/xml-%23000000.svg?style=for-the-badge&logo=xml&logoColor=white',
-  },
-  {
-    alt: 'PHP',
-    class: 'badge',
-    src: 'https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white',
-  },
-  {
-    alt: 'Python',
-    class: 'badge',
-    src: 'https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54',
-  },
-  {
-    alt: 'Bash',
-    class: 'badge',
-    src: 'https://img.shields.io/badge/bash_script-%23121011.svg?style=for-the-badge&logo=gnu-bash&logoColor=white',
-  },
-  {
-    alt: 'NodeJS',
-    class: 'badge',
-    src: 'https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white',
+    src: 'https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white',
   },
   {
     alt: 'React',
     class: 'badge',
-    src: 'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
+    src: 'https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
   },
   {
     alt: 'Vite',
     class: 'badge',
-    src: 'https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white',
+    src: 'https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=vite&logoColor=white',
   },
+
+  // --- BACKEND & LENGUAJES ---
+  {
+    alt: 'Java',
+    class: 'badge',
+    src: 'https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white',
+  },
+  {
+    alt: 'PHP',
+    class: 'badge',
+    src: 'https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white',
+  },
+  {
+    alt: 'Python',
+    class: 'badge',
+    src: 'https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=FFD43B',
+  },
+  {
+    alt: 'NodeJS',
+    class: 'badge',
+    src: 'https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=node.js&logoColor=white',
+  },
+
+  // --- DATOS & SISTEMAS ---
+  {
+    alt: 'SQL',
+    class: 'badge',
+    src: 'https://img.shields.io/badge/SQL-005963?style=for-the-badge&logo=databricks&logoColor=white',
+  },
+  {
+    alt: 'XML',
+    class: 'badge',
+    src: 'https://img.shields.io/badge/XML-555555?style=for-the-badge&logo=codeforces&logoColor=white',
+  },
+  {
+    alt: 'Bash',
+    class: 'badge',
+    src: 'https://img.shields.io/badge/Bash-121011?style=for-the-badge&logo=gnu-bash&logoColor=white',
+  },
+
+  // --- TOOLS & DESKTOP ---
   {
     alt: 'Electron',
     class: 'badge',
-    src: 'https://img.shields.io/badge/electron-%23000000.svg?style=for-the-badge&logo=electron&logoColor=white',
-  },  
+    src: 'https://img.shields.io/badge/Electron-47848F?style=for-the-badge&logo=electron&logoColor=white',
+  },
   {
     alt: 'Git',
     class: 'badge',
-    src: 'https://img.shields.io/badge/git-%23F05032.svg?style=for-the-badge&logo=git&logoColor=white',
+    src: 'https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white',
   },
-  {
-    alt: 'mySQL',
-    class: 'badge',
-    src: 'https://img.shields.io/badge/MySQL-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white',
-  }
 ];
 
 export default function GithubStats({ currentColor }: GithubStatsProps) {
